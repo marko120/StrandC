aa4b6693f7079f47a42a0196a05735ef0cb14c5f
farkasma <farkasma@310-12>
2026-05-04 14:36:43 +0200

    fuggvenyek 4.feladat

--- StrandC/Program.cs
+++ StrandC/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace StrandC
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace StrandC
 {
-    internal class Program
+    public class Furdo
+
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        public string Név { get; set; }
+        public string Cím { get; set; }
+        public int Ár { get; private set; }
+        public int Vízhőfok { get; private set; }
+
+
+
+        public Furdo(string sor)
+        { 
+            string[] adatok = sor.Split(';');
+            Név = adatok[0];
+            Cím = adatok[1];
+            Ár = int.Parse(adatok[2]);
+            Vízhőfok = int.Parse(adatok[3]);
         }
+
+
+        public string IRSZ()
+        {
+            return Cím.Split(" ")[0];
+        }
+
+
+        public string Telepules()
+        {
+            return Cím.Split("  ")[1].Split(" ")[0];
+        }
+
     }
 }
